4685ae3eb20ac33e729f6ffb557e2d3a1ae84f87
19aliaksandr90 <56079976+19aliaksandr90@users.noreply.github.com>
2021-02-16 14:24:26 +0300

    gf-3 Create Basic components (#4)
    * gf-3 change eslint rules, add editorconfig file, install dependencies
    
    * gf-3 Create basic components
    
    Co-authored-by: Aliaksandr Samusevich <aliaksandr_samusevich@marpaihealth.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
+import Navbar from './layouts/Navbar';
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <Navbar title="Github Finder" icon="fab fa-github" />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
---
+++ src/layouts/Navbar/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+const Navbar = ({ title, icon }) => (
+  <nav className="navbar bg-primary">
+    <h1>
+      <i className={icon} /> {title}
+    </h1>
+  </nav>
+);
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
+
+export default Navbar;
